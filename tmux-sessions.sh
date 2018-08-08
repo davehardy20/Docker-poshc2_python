1c435a37a43d05f1145283331cf597d69014d5a7
Dave Hardy <davehardy20@gmail.com>
2018-08-08 09:34:35 +0100

    Fix Sessions and WIndows

--- tmux-sessions.sh
+++ tmux-sessions.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-SESSION=poshc2
-
+SESSION=Red_Team
+set mouse on
 tmux -2 new -d -s $SESSION
 
 # Setup a window for a PoshC2
-tmux new-window -t $SESSION:1 -n 'poshc2'
+tmux new-window -t $SESSION:1
+tmux select-window -t $SESSION:1
 tmux split-window -h
 tmux select-pane -t 0
 tmux send-keys "python C2Server.py" C-m
